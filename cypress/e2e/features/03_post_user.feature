6e7afb3232dc4d732168bac16cb265390ca584b8
Feyza Ocakdan <ocakdanfeyza@gmail.com>
2023-10-10 12:00:36 +0300

    fixture file adding

--- cypress/e2e/features/03_post_user.feature
+++ cypress/e2e/features/03_post_user.feature
@@ -3,4 +3,6 @@ Feature: Create a Todo Item
   Scenario: Create a new todo item
     Given I make a POST request to url
     Then the response should have a status code 201
-  
+    And user verifies the response body data post
+
+     
--- cypress/integration/03_post_user.js
+++ cypress/integration/03_post_user.js
@@ -1,11 +1,17 @@
 import { Given, When, Then, DataTable } from  "cypress-cucumber-preprocessor/steps";
-
 const { faker } = require('@faker-js/faker');
 
 let response;
 let email=faker.internet.email();
+let payload;
 
 Given('I make a POST request to url',() => {
+
+
+    cy.fixture('createuser.json').then((fixturePayload)=>{
+        payload =fixturePayload
+
+
     cy.request({
         method:'POST',
         url: "https://gorest.co.in/public/v2/users",
@@ -13,21 +19,34 @@ Given('I make a POST request to url',() => {
             'Authorization':"Bearer f8a3394584c1609af76a4607c962152f664684a2cdd3bf17c3b959e26e11ced3"
         },
         body:{
-            "id": 8787872535,
-            "name": "bilgeOcakdan",
+            "id": payload.id,
+            "name":payload.name,
             "email": email,
-            "gender": "female",
-            "status": "active"
+            "gender":payload.gender,
+            "status": payload.status
           },
         
     }).then((res)=>{
         response=res
     })
+
+
+})
 });
 
 Then('the response should have a status code {int}', (statusCode) => {
     expect(response.status).to.eq(statusCode)
 });
+
+And('user verifies the response body data post',()=>{
+    expect(response.body.email).to.eq(email)
+    expect(response.body).has.property('gender',payload.gender)
+    expect(response.body).has.property('status',payload.status)
+    
+})
+
+
+
 
 
 
